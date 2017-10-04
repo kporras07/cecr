f4c3cef851191456a2aa9a6cd60c231de0fa5932
Kevin Porras <kporras07@gmail.com>
2017-10-03 20:17:29 -0600

    Update install script.

--- scripts/cecr_local_install.sh
+++ scripts/cecr_local_install.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-DRUSH="./vendor/bin/drush"
-SITE_ALIAS="@cecr.cecr.dev"
 SITE_UUID="38a4f0bd-03fa-41a2-ba03-13282e717797"
-$DRUSH $SITE_ALIAS cc drush
+ahoy drush cc drush
 echo "Installing..."
-$DRUSH $SITE_ALIAS si cecr --account-pass=admin -y
+ahoy drush si cecr --account-pass=admin -y
 echo "Set site uuid..."
-$DRUSH $SITE_ALIAS config-set "system.site" uuid "$SITE_UUID" -y
+ahoy drush config-set "system.site" uuid "$SITE_UUID" -y
 echo "Importing config..."
-if [ -f ./config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS cim -y ; fi
+if [ -f ./config/sync/core.extension.yml ] ; then ahoy drush cim -y ; fi
 echo "Cleaning cache..."
-$DRUSH $SITE_ALIAS cr
+ahoy drush cr
